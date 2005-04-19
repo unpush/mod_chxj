3d5f9f6b47db6ab5401ced35ff061ef6f557293b
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2005-04-19 14:40:32 +0000

    *** empty log message ***
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@186 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_chtml30.c
+++ src/chxj_chtml30.c
@@ -47,7 +47,7 @@ static char* s_chtml30_end_input_tag    (Chtml30* chtml, Node* node);
 static char* s_chtml30_start_center_tag (Chtml30* chtml, Node* node);
 static char* s_chtml30_end_center_tag   (Chtml30* chtml, Node* node);
 static char* s_chtml30_start_hr_tag     (Chtml30* chtml, Node* node);
-static char* chtml30_end_hr_tag       (Chtml30* chtml, Node* node);
+static char* s_chtml30_end_hr_tag       (Chtml30* chtml, Node* node);
 static char* chtml30_start_img_tag    (Chtml30* chtml, Node* node);
 static char* chtml30_end_img_tag      (Chtml30* chtml, Node* node);
 static char* chtml30_start_select_tag (Chtml30* chtml, Node* node);
@@ -302,7 +302,7 @@ s_chtml30_node_exchange(Chtml30* chtml30, Node* node, int indent)
     if (strcasecmp(name, "hr") == 0) 
     {
       s_chtml30_start_hr_tag  (chtml30, child);
-      chtml30_end_hr_tag    (chtml30, child);
+      s_chtml30_end_hr_tag    (chtml30, child);
     }
     /*------------------------------------------------------------------------*/
     /* <CENTER>                                                               */
@@ -1469,7 +1469,7 @@ s_chtml30_start_hr_tag(Chtml30* chtml30, Node* node)
  * @return The conversion result is returned.
  */
 static char*
-chtml30_end_hr_tag(Chtml30* chtml30, Node* child) 
+s_chtml30_end_hr_tag(Chtml30* chtml30, Node* child) 
 {
   return chtml30->out;
 }
