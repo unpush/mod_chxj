3076fab45044372c92a73b73908d46a75cb38db8
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2005-04-19 14:10:45 +0000

    *** empty log message ***
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@157 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_chtml20.c
+++ src/chxj_chtml20.c
@@ -50,7 +50,7 @@ static char* s_chtml20_start_hr_tag     (Chtml20* chtml, Node* node);
 static char* s_chtml20_end_hr_tag       (Chtml20* chtml, Node* node);
 static char* s_chtml20_start_img_tag    (Chtml20* chtml, Node* node);
 static char* s_chtml20_end_img_tag      (Chtml20* chtml, Node* node);
-static char* chtml20_start_select_tag (Chtml20* chtml, Node* node);
+static char* s_chtml20_start_select_tag (Chtml20* chtml, Node* node);
 static char* chtml20_end_select_tag   (Chtml20* chtml, Node* node);
 static char* chtml20_start_option_tag (Chtml20* chtml, Node* node);
 static char* chtml20_end_option_tag   (Chtml20* chtml, Node* node);
@@ -327,7 +327,7 @@ s_chtml20_node_exchange(Chtml20* chtml20, Node* node, int indent)
     else
     if (strcasecmp(name, "select") == 0)
     {
-      chtml20_start_select_tag(chtml20, child);
+      s_chtml20_start_select_tag(chtml20, child);
       s_chtml20_node_exchange   (chtml20, child, indent+1);
       chtml20_end_select_tag  (chtml20, child);
     }
@@ -1599,7 +1599,7 @@ s_chtml20_end_img_tag(Chtml20* chtml20, Node* child)
  * @return The conversion result is returned.
  */
 static char*
-chtml20_start_select_tag(Chtml20* chtml20, Node* child)
+s_chtml20_start_select_tag(Chtml20* chtml20, Node* child)
 {
   Doc* doc = chtml20->doc;
   request_rec* r = doc->r;
