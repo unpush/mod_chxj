bfe8dced32ecba50c08c80f1624b367ba291f840
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2005-04-19 14:49:46 +0000

    *** empty log message ***
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@192 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_chtml30.c
+++ src/chxj_chtml30.c
@@ -53,7 +53,7 @@ static char* s_chtml30_end_img_tag      (Chtml30* chtml, Node* node);
 static char* s_chtml30_start_select_tag (Chtml30* chtml, Node* node);
 static char* s_chtml30_end_select_tag   (Chtml30* chtml, Node* node);
 static char* s_chtml30_start_option_tag (Chtml30* chtml, Node* node);
-static char* chtml30_end_option_tag   (Chtml30* chtml, Node* node);
+static char* s_chtml30_end_option_tag   (Chtml30* chtml, Node* node);
 static char* chtml30_start_div_tag    (Chtml30* chtml, Node* node);
 static char* chtml30_end_div_tag      (Chtml30* chtml, Node* node);
 static void chxj_init_chtml30(Chtml30* chtml, Doc* doc, request_rec* r, device_table* spec);
@@ -341,7 +341,7 @@ s_chtml30_node_exchange(Chtml30* chtml30, Node* node, int indent)
     {
       s_chtml30_start_option_tag(chtml30, child);
       s_chtml30_node_exchange   (chtml30, child, indent+1);
-      chtml30_end_option_tag  (chtml30, child);
+      s_chtml30_end_option_tag  (chtml30, child);
     }
     /*------------------------------------------------------------------------*/
     /* <DIV>                                                                  */
@@ -1754,7 +1754,7 @@ s_chtml30_start_option_tag(Chtml30* chtml30, Node* child)
  * @return The conversion result is returned.
  */
 static char*
-chtml30_end_option_tag(Chtml30* chtml30, Node* child)
+s_chtml30_end_option_tag(Chtml30* chtml30, Node* child)
 {
   /* Don't close */
   return chtml30->out;
