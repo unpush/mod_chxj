aae79c77b89e0a3f8a190abf87856cf192d92b80
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2005-04-19 14:44:40 +0000

    *** empty log message ***
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@191 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_chtml30.c
+++ src/chxj_chtml30.c
@@ -52,7 +52,7 @@ static char* s_chtml30_start_img_tag    (Chtml30* chtml, Node* node);
 static char* s_chtml30_end_img_tag      (Chtml30* chtml, Node* node);
 static char* s_chtml30_start_select_tag (Chtml30* chtml, Node* node);
 static char* s_chtml30_end_select_tag   (Chtml30* chtml, Node* node);
-static char* chtml30_start_option_tag (Chtml30* chtml, Node* node);
+static char* s_chtml30_start_option_tag (Chtml30* chtml, Node* node);
 static char* chtml30_end_option_tag   (Chtml30* chtml, Node* node);
 static char* chtml30_start_div_tag    (Chtml30* chtml, Node* node);
 static char* chtml30_end_div_tag      (Chtml30* chtml, Node* node);
@@ -339,7 +339,7 @@ s_chtml30_node_exchange(Chtml30* chtml30, Node* node, int indent)
     else
     if (strcasecmp(name, "option") == 0)
     {
-      chtml30_start_option_tag(chtml30, child);
+      s_chtml30_start_option_tag(chtml30, child);
       s_chtml30_node_exchange   (chtml30, child, indent+1);
       chtml30_end_option_tag  (chtml30, child);
     }
@@ -1693,7 +1693,7 @@ s_chtml30_end_select_tag(Chtml30* chtml30, Node* child)
  * @return The conversion result is returned.
  */
 static char*
-chtml30_start_option_tag(Chtml30* chtml30, Node* child)
+s_chtml30_start_option_tag(Chtml30* chtml30, Node* child)
 {
   Doc* doc = chtml30->doc;
   request_rec* r = doc->r;
