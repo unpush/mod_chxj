4b0a25eabbdde456ad0a2acf3329bb0e16bf87b9
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-06-08 08:07:25 +0000

    *** empty log message ***
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@287 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_chtml10.c
+++ src/chxj_chtml10.c
@@ -983,12 +983,10 @@ s_chtml10_start_form_tag(chtml10_t* chtml10, Node* node)
   /*--------------------------------------------------------------------------*/
   for (attr = qs_get_attr(doc,node);
        attr;
-       attr = qs_get_next_attr(doc,attr)) 
-  {
+       attr = qs_get_next_attr(doc,attr)) {
     char* name = qs_get_attr_name(doc,attr);
     char* value = qs_get_attr_value(doc,attr);
-    if (strcasecmp(name, "action") == 0) 
-    {
+    if ((*name == 'a' || *name == 'A') && strcasecmp(name, "action") == 0) {
       /*----------------------------------------------------------------------*/
       /* CHTML 1.0                                                            */
       /*----------------------------------------------------------------------*/
