888827ea7ee0eb63e6d0342812516ea2544d411f
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-06-09 07:14:07 +0000

    *** empty log message ***
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@303 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_chtml10.c
+++ src/chxj_chtml10.c
@@ -1346,8 +1346,7 @@ s_chtml10_start_img_tag(chtml10_t* chtml10, Node* node)
                       chtml10->out, " height=\"",value,"\"", NULL);
     }
     else
-    if (strcasecmp(name, "hspace") == 0) 
-    {
+    if (strcasecmp(name, "hspace") == 0) {
       /*----------------------------------------------------------------------*/
       /* CHTML 1.0                                                            */
       /*----------------------------------------------------------------------*/
