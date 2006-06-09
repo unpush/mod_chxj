46e3ce23deede68a3df4215cd2a71825882b3a54
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-06-09 11:32:24 +0000

    *** empty log message ***
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@380 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_hdml.c
+++ src/chxj_hdml.c
@@ -920,8 +920,7 @@ s_hdml_start_a_tag(hdml_t* hdml, Node* node)
   /*------------------------------------*/
   for (attr = qs_get_attr(doc,node);
        attr;
-       attr = qs_get_next_attr(doc,attr)) 
-  {
+       attr = qs_get_next_attr(doc,attr)) {
     char* name  = qs_get_attr_name(doc,attr);
     char* value = qs_get_attr_value(doc,attr);
     if (strcasecmp(name, "name") == 0) 
