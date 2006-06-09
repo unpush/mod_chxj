7b80b1daa70db674b229b6ba0ed96edc9393e1cf
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-06-09 07:29:02 +0000

    *** empty log message ***
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@326 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_chtml10.c
+++ src/chxj_chtml10.c
@@ -1561,8 +1561,7 @@ s_chtml10_start_div_tag(chtml10_t* chtml10, Node* child)
   chtml10->out = apr_pstrcat(r->pool, chtml10->out, "<div", NULL);
   for (attr = qs_get_attr(doc,child);
        attr;
-       attr = qs_get_next_attr(doc,attr)) 
-  {
+       attr = qs_get_next_attr(doc,attr)) {
     char* nm  = qs_get_attr_name(doc,attr);
     char* val = qs_get_attr_value(doc,attr);
 
