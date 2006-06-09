8a581aaa277abcdb5840eebcee7f6d1c6e81b602
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-06-09 17:13:51 +0000

    *** empty log message ***
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@430 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_hdml.c
+++ src/chxj_hdml.c
@@ -2030,7 +2030,10 @@ s_hdml_start_img_tag(hdml_t* hdml, Node* node)
   s_output_to_hdml_out(hdml, "<img");
 
   /* Get Attributes */
-  for (attr = qs_get_attr(doc,node); attr != NULL; attr = qs_get_next_attr(doc,attr)) {
+  for (attr = qs_get_attr(doc,node);
+       attr; 
+       attr = qs_get_next_attr(doc,attr)) {
+
     char* name  = qs_get_attr_name(doc,attr);
     char* value = qs_get_attr_value(doc,attr);
 
