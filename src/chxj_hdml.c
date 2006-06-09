5250b0178700c1afb1c27a587a67cb61e73d14f7
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-06-09 07:46:40 +0000

    *** empty log message ***
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@345 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_hdml.c
+++ src/chxj_hdml.c
@@ -433,8 +433,7 @@ s_hdml_node_exchange(hdml_t* hdml, Node* node,  int indent)
     /* <IMG>                                                                  */
     /*------------------------------------------------------------------------*/
     else
-    if (strcasecmp(name, "img") == 0) 
-    {
+    if (strcasecmp(name, "img") == 0) {
       hdml->out = s_hdml_start_img_tag(hdml, child);
       hdml->hdml_br_flag = 0;
       hdml->out = s_hdml_end_img_tag  (hdml, child);
