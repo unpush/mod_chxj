fd8444888170d6b01fda29d517e5c288256950f4
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-06-09 08:20:05 +0000

    *** empty log message ***
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@361 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_hdml.c
+++ src/chxj_hdml.c
@@ -411,7 +411,7 @@ s_hdml_node_exchange(hdml_t* hdml, Node* node,  int indent)
     /* <SELECT>                                                               */
     /*------------------------------------------------------------------------*/
     else
-    if (strcasecmp(name, "select") == 0) {
+    if ((*name == 's' || *name == 'S') && strcasecmp(name, "select") == 0) {
       hdml->out = s_hdml_start_select_tag(hdml, child);
       hdml->hdml_br_flag = 0;
       hdml->out = s_hdml_node_exchange(hdml, child,indent+1);
@@ -421,7 +421,7 @@ s_hdml_node_exchange(hdml_t* hdml, Node* node,  int indent)
     /* <OPTION>                                                               */
     /*------------------------------------------------------------------------*/
     else
-    if (strcasecmp(name, "option") == 0) {
+    if ((*name == 'o' || *name == 'O') && strcasecmp(name, "option") == 0) {
       hdml->out = s_hdml_start_option_tag(hdml, child);
       hdml->hdml_br_flag = 0;
       hdml->out = s_hdml_node_exchange(hdml, child,indent+1);
