352e74e3af45427d55828bbc0598eff4750c386c
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-06-11 09:03:22 +0000

    * Debug off
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@471 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/qs_parse_string.c
+++ src/qs_parse_string.c
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define DEBUG
 #include <stdio.h>
 #include <strings.h>
 #include "httpd.h"
