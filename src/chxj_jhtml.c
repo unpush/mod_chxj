db6fa3aadb7d53d9e34eb46a914d48f54815dc97
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-06-13 19:20:23 +0000

    *** empty log message ***
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@574 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_jhtml.c
+++ src/chxj_jhtml.c
@@ -1610,7 +1610,7 @@ s_jhtml_start_hr_tag(jhtml_t* jhtml, Node* node)
        attr = qs_get_next_attr(doc,attr)) {
     char* name = qs_get_attr_name(doc,attr);
     char* value = qs_get_attr_value(doc,attr);
-    if (strcasecmp(name, "align") == 0) {
+    if ((*name == 'a' || *name == 'A') && strcasecmp(name, "align") == 0) {
       /*----------------------------------------------------------------------*/
       /* CHTML 1.0                                                            */
       /*----------------------------------------------------------------------*/
