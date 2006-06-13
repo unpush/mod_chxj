35df98e7922d25828403770f7537e1aaa2c59fab
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-06-13 19:29:04 +0000

    *** empty log message ***
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@599 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_jhtml.c
+++ src/chxj_jhtml.c
@@ -1893,14 +1893,14 @@ s_jhtml_start_option_tag(jhtml_t* jhtml, Node* child)
     char* nm  = qs_get_attr_name(doc,attr);
     char* val = qs_get_attr_value(doc,attr);
 
-    if (strcasecmp(nm, "selected") == 0) {
+    if ((*name == 's' || *name == 'S') && strcasecmp(nm, "selected") == 0) {
       /*----------------------------------------------------------------------*/
       /* CHTML 1.0 version 2.0                                                */
       /*----------------------------------------------------------------------*/
       selected = apr_pstrdup(r->pool, val);
     }
     else
-    if (strcasecmp(nm, "value") == 0) {
+    if ((*name == 'v' || *name == 'V') && strcasecmp(nm, "value") == 0) {
       /*----------------------------------------------------------------------*/
       /* CHTML 1.0 version 2.0                                                */
       /*----------------------------------------------------------------------*/
