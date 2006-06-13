5d082a5faf63c4ad9285a7508272e756acc326ed
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-06-13 19:18:56 +0000

    *** empty log message ***
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@568 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_jhtml.c
+++ src/chxj_jhtml.c
@@ -1418,6 +1418,7 @@ s_jhtml_start_input_tag(jhtml_t* jhtml, Node* node)
   }
 
   jhtml->out = apr_pstrcat(r->pool, jhtml->out, " >", NULL);
+
   return jhtml->out;
 }
 
@@ -1606,8 +1607,7 @@ s_jhtml_start_hr_tag(jhtml_t* jhtml, Node* node)
  
   for (attr = qs_get_attr(doc,node);
        attr; 
-       attr = qs_get_next_attr(doc,attr)) 
-  {
+       attr = qs_get_next_attr(doc,attr)) {
     char* name = qs_get_attr_name(doc,attr);
     char* value = qs_get_attr_value(doc,attr);
     if (strcasecmp(name, "align") == 0) 
