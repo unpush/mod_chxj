718d830fee8b2d94c197ccaa743561badfb133f3
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-06-16 19:49:50 +0000

    * Writing is changed.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@935 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_tag_util.c
+++ src/chxj_tag_util.c
@@ -544,9 +544,8 @@ qs_get_destlang_attr(Doc* doc, Node* tag, request_rec* r)
   Attr*        attr;
 
   for (attr = qs_get_attr(doc,tag);
-       attr != NULL; 
-       attr = qs_get_next_attr(doc,attr))
-  {
+       attr; 
+       attr = qs_get_next_attr(doc,attr)) {
     char* name  = qs_get_attr_name(doc,attr);
     char* value = qs_get_attr_value(doc,attr);
 
