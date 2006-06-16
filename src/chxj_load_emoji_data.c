937b6a744266200e9da3017a7997f98f8bcf5ef0
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-06-16 14:00:01 +0000

    * Writing is changed.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@819 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_load_emoji_data.c
+++ src/chxj_load_emoji_data.c
@@ -135,11 +135,8 @@ s_load_emoji_set_tag(
     }
     else
     if ((*name == 'e' || *name == 'E') && strcasecmp(name, "ezweb") == 0) {
-      rtn = s_load_emoji_ezweb_tag(doc, p, em, child);
-      if (rtn != NULL)
-      {
+      if (rtn = s_load_emoji_ezweb_tag(doc, p, em, child)) 
         return rtn;
-      }
     }
     else
     if (strcasecmp(name, "jphone") == 0)
