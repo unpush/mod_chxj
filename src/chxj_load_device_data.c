4009eb1d361bcec7a00901f90cfc70ad29ca1a9e
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-06-16 11:21:08 +0000

    * Writing is changed.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@767 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_load_device_data.c
+++ src/chxj_load_device_data.c
@@ -324,12 +324,10 @@ s_set_device_data(Doc* doc, apr_pool_t* p, device_table_list_t* dtl, Node* node)
     else
     if (strcasecmp(name, "wp_heigh") == 0) {
       Node* ch = qs_get_child_node(doc, child);
-      if (ch != NULL && strcasecmp(qs_get_node_name(doc,ch), "text") == 0) 
-      {
+      if (ch && strcasecmp(qs_get_node_name(doc,ch), "text") == 0) {
         char *vv = qs_get_node_value(doc,ch);
         int ii;
-        for (ii=0; ii<strlen(vv); ii++) 
-        {
+        for (ii=0; ii<strlen(vv); ii++) {
           if ((vv[ii] >= '1' && vv[ii] <= '9') || vv[ii] == '0') 
           {
             continue;
