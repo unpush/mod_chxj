b405528361a97910c8d168124d4c5bb32731b796
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-06-16 11:09:13 +0000

    * Writing is changed.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@720 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_load_device_data.c
+++ src/chxj_load_device_data.c
@@ -156,8 +156,7 @@ s_set_device_data(Doc* doc, apr_pool_t* p, device_table_list_t* dtl, Node* node)
       Node* ch = qs_get_child_node(doc, child);
       if (ch &&  strcasecmp(qs_get_node_name(doc,ch), "text") == 0) {
         char* vv = qs_get_node_value(doc, ch);
-        if (strcasecmp(vv, "xhtml_mobile_1_0") == 0) 
-        {
+        if (strcasecmp(vv, "xhtml_mobile_1_0") == 0) {
           dt->html_spec_type = CHXJ_SPEC_XHtml_Mobile_1_0;
         }
         else
