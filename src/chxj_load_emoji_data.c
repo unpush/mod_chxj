f03604611407b295a75e765280b7a035d19c183f
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-06-16 14:16:16 +0000

    * Writing is changed.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@870 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_load_emoji_data.c
+++ src/chxj_load_emoji_data.c
@@ -410,8 +410,7 @@ s_load_emoji_jphone_tag(
 
   for (child = qs_get_child_node(doc, node);
        child ;
-       child = qs_get_next_node(doc, child))
-  {
+       child = qs_get_next_node(doc, child)) {
     char* name  = qs_get_node_name(doc, child);
     if (strcasecmp(name, "string") == 0) 
     {
