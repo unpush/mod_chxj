dc16134678aa4be5c5a522a7a11eb945bd78b11e
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-06-16 11:28:47 +0000

    * Writing is changed.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@778 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_load_device_data.c
+++ src/chxj_load_device_data.c
@@ -375,8 +375,7 @@ s_set_device_data(Doc* doc, apr_pool_t* p, device_table_list_t* dtl, Node* node)
       }
     }
     else
-    if (strcasecmp(name, "dpi_heigh") == 0) 
-    {
+    if (strcasecmp(name, "dpi_heigh") == 0) {
       Node* ch = qs_get_child_node(doc, child);
       if (ch != NULL && strcasecmp(qs_get_node_name(doc,ch), "text") == 0) 
       {
