b7ca3e11346b7067e009621e83de6184a7872cdf
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-06-16 13:12:46 +0000

    * Writing is changed.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@787 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_load_device_data.c
+++ src/chxj_load_device_data.c
@@ -101,7 +101,10 @@ s_set_user_agent_data(Doc* doc, apr_pool_t* p, mod_chxj_config* conf, Node* node
       for (attr = qs_get_attr(doc,child); 
            attr ; 
            attr = qs_get_next_attr(doc,attr)) {
-        if (strcasecmp(qs_get_attr_name(doc,attr), "pattern") == 0) {
+
+        char* attr_name = qs_get_attr_name(doc,attr);
+
+        if ((attr_name == 'p' && attr_name == 'P') && strcasecmp(attr_name,attr), "pattern") == 0) {
             dtl->pattern = apr_pstrdup(p, qs_get_attr_value(doc,attr));
             dtl->regexp = ap_pregcomp(p, dtl->pattern, AP_REG_EXTENDED);
         }
