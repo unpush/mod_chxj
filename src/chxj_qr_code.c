39133ecf1fa4720fc7221404f627c739034ffcb7
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-06-18 10:15:16 +0000

    * Writing is changed.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@1040 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_qr_code.c
+++ src/chxj_qr_code.c
@@ -2193,8 +2193,8 @@ s_count_same_module(qr_ver_t ver, char* dst[])
   for (xx=0; xx<module_count; xx++) {
     prev_value = -1;
     same_count = 0;
-    for (yy=0; yy<module_count; yy++)
-    {
+
+    for (yy=0; yy<module_count; yy++) {
       if (prev_value == (unsigned char)dst[yy][xx])
       {
         same_count++;
