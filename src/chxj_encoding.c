962511cd5ee1e4cccb8444a1c094218911f408a0
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-06-24 05:16:35 +0000

    * change writting.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@1140 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_encoding.c
+++ src/chxj_encoding.c
@@ -111,8 +111,7 @@ chxj_rencoding(request_rec *r, const char* src, apr_size_t* len)
   mod_chxj_config* dconf;
   chxjconvrule_entry* entryp;
 
-  ap_log_rerror(
-    APLOG_MARK,APLOG_DEBUG, 0, r, "start chxj_rencoding()");
+  DBG(r,"start chxj_rencoding()");
 
   dconf = ap_get_module_config(r->per_dir_config, &chxj_module);
 
