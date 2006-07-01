82c2da35d1d80fb401a6198b4c04106c6fe5ae77
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-07-01 06:13:28 +0000

    * change writting.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@1400 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_chtml10.c
+++ src/chxj_chtml10.c
@@ -2320,8 +2320,11 @@ s_chtml10_start_div_tag(void* pdoc, Node* child)
   for (attr = qs_get_attr(doc,child);
        attr;
        attr = qs_get_next_attr(doc,attr)) {
-    char* nm  = qs_get_attr_name(doc,attr);
-    char* val = qs_get_attr_value(doc,attr);
+    char* nm;
+    char* val;
+
+    nm  = qs_get_attr_name(doc,attr);
+    val = qs_get_attr_value(doc,attr);
 
     if ((*nm == 'a' || *nm == 'A') && strcasecmp(nm, "align") == 0) {
       /*----------------------------------------------------------------------*/
