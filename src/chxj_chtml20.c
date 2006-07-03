b32f87950928fe39338b951c60c2e46ea60802bd
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-07-03 00:53:19 +0000

    * change writting.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@1488 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_chtml20.c
+++ src/chxj_chtml20.c
@@ -2200,7 +2200,8 @@ s_chtml20_start_select_tag(void* pdoc, Node* child)
   }
 
   if (size)
-    chtml20->out = apr_pstrcat(r->pool, chtml20->out, " size=\"",size,"\"", NULL);
+    chtml20->out = apr_pstrcat(r->pool, 
+                               chtml20->out, " size=\"",size,"\"", NULL);
 
   if (name)
     chtml20->out = apr_pstrcat(r->pool, chtml20->out, " name=\"",name,"\"", NULL);
