e5e7398478bc0b27f1f567cd5736d89ecf22aca4
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-07-03 00:48:12 +0000

    * change writting.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@1478 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_chtml20.c
+++ src/chxj_chtml20.c
@@ -1987,7 +1987,7 @@ s_chtml20_start_img_tag(void* pdoc, Node* node)
                         chtml20->out, " src=\"",value,"\"", NULL);
 #else
         chtml20->out = apr_pstrcat(r->pool, 
-                        chtml20->out, " src=\"", 
+                                   chtml20->out, " src=\"", 
                         chxj_img_conv(r, spec,value), NULL);
         chtml20->out = apr_pstrcat(r->pool, 
                                    chtml20->out,
