7db80a8f6713f862e7ca96dbe72ea443d92d2996
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-07-03 01:06:00 +0000

    * change writting.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@1524 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_chtml20.c
+++ src/chxj_chtml20.c
@@ -2416,7 +2416,8 @@ s_chtml20_start_div_tag(void* pdoc, Node* child)
 
   chtml20->out = apr_pstrcat(r->pool, 
                              chtml20->out, 
-                             ">", NULL);
+                             ">", 
+                             NULL);
 
   return chtml20->out;
 }
