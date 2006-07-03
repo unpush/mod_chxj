d4007597ebfb7e1fec5ba029a6335a67d22d2124
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-07-03 00:21:34 +0000

    * change writting.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@1450 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_chtml20.c
+++ src/chxj_chtml20.c
@@ -1375,8 +1375,13 @@ s_chtml20_start_form_tag(void* pdoc, Node* node)
   for (attr = qs_get_attr(doc,node);
        attr;
        attr = qs_get_next_attr(doc,attr)) {
-    char* name = qs_get_attr_name(doc,attr);
-    char* value = qs_get_attr_value(doc,attr);
+
+    char* name;
+    char* value;
+
+    name  = qs_get_attr_name(doc,attr);
+    value = qs_get_attr_value(doc,attr);
+
     if (strcasecmp(name, "action") == 0) {
       /*----------------------------------------------------------------------*/
       /* CHTML 1.0                                                            */
