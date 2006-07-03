c923b113f50b6e60e95bfbf0f6bf439e6b12b95c
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-07-03 12:01:13 +0000

    * change writting.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@1562 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_chtml20.c
+++ src/chxj_chtml20.c
@@ -1891,7 +1891,7 @@ s_chtml20_start_hr_tag(void* pdoc, Node* node)
         /* CHTML 1.0                                                          */
         /*--------------------------------------------------------------------*/
         chtml20->out = apr_pstrcat(r->pool,
-                          chtml20->out, 
+                                   chtml20->out, 
                           " align=\"", value, "\" ", NULL);
       }
       break;
