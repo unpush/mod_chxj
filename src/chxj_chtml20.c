51d1d1eaa3ca6d45daa12b05731ce099d2d27b70
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-07-03 00:46:20 +0000

    * change writting.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@1472 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_chtml20.c
+++ src/chxj_chtml20.c
@@ -2002,7 +2002,8 @@ s_chtml20_start_img_tag(void* pdoc, Node* node)
         /*--------------------------------------------------------------------*/
         chtml20->out = apr_pstrcat(r->pool, 
                                    chtml20->out, 
-                                   " align=\"",value,"\"", NULL);
+                                   " align=\"",
+                                   value,"\"", NULL);
       }
       else
       if (strcasecmp(name, "alt"   ) == 0) {
