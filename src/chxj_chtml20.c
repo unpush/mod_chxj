294f31d973e72eef658f96cf8b370e9ec553a42c
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-07-03 00:58:43 +0000

    * change writting.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@1506 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_chtml20.c
+++ src/chxj_chtml20.c
@@ -2314,7 +2314,8 @@ s_chtml20_start_option_tag(void* pdoc, Node* child)
     chtml20->out = apr_pstrcat(r->pool, 
                                chtml20->out, 
                                " value=\"",
-                               value,"\"", NULL);
+                               value,
+                               "\"", NULL);
   else
     chtml20->out = apr_pstrcat(r->pool, chtml20->out, " value=\"\"", NULL);
 
