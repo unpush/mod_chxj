61d0a2383e6184da2ac3a00e696da5c0cccb0138
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-07-03 00:59:46 +0000

    * change writting.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@1511 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_chtml20.c
+++ src/chxj_chtml20.c
@@ -2324,7 +2324,8 @@ s_chtml20_start_option_tag(void* pdoc, Node* child)
                                NULL);
 
   if (selected)
-    chtml20->out = apr_pstrcat(r->pool, chtml20->out, " selected ", NULL);
+    chtml20->out = apr_pstrcat(r->pool, 
+                               chtml20->out, " selected ", NULL);
 
   chtml20->out = apr_pstrcat(r->pool, chtml20->out, ">", NULL);
 
