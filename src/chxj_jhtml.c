62b242891c0352a6b3f83b715dacb5611868a0e8
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2006-07-04 22:51:13 +0000

    * change writting.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@1696 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_jhtml.c
+++ src/chxj_jhtml.c
@@ -1734,9 +1734,13 @@ s_jhtml_end_ol_tag(void* pdoc, Node* child)
 static char*
 s_jhtml_start_p_tag(void* pdoc, Node* node) 
 {
-  jhtml_t*      jhtml = GET_JHTML(pdoc);
-  Doc*          doc   = jhtml->doc;
-  request_rec*  r     = doc->r;
+  jhtml_t*      jhtml;
+  Doc*          doc;
+  request_rec*  r;
+
+  jhtml = GET_JHTML(pdoc);
+  doc   = jhtml->doc;
+  r     = doc->r;
 
   jhtml->out = apr_pstrcat(r->pool, jhtml->out, "<p>", NULL);
 
