4a210c5e8bb904b7d68dc9dfe524019c15454581
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-02-07 05:04:59 +0000

    * added sample site.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@1832 1a406e8e-add9-4483-a2c8-d8cac5b7c224

---
+++ sample_site/euc/get1_euc.php
@@ -0,0 +1,34 @@
+<?php
+header("Cache-Control: no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+?>
+<html>
+  <head>
+  </head>
+  <body>
+<?php
+  $data = $_GET["ü¡"];
+?>
+<center>
+³¨Ê¸»úÆþÎÏ¤ÎŽÃŽ½ŽÄ(EUCJP¤ÇGET)
+</center>
+<hr>
+    <form name="ü¡" method="get" action="get1_euc.php">
+      <input type="text" name="ü¡" value="<?=${data}?>" />
+      <input type="submit" value="þþ" />
+    </form>
+Ž¹ŽÞŽ¯ŽÄ¤µ¤ì¤¿ÃÍ¤Ï¡¢¡¢¡¢<br>
+[
+<?php
+  echo "HEX:[" . bin2hex($data) . "]<br>";
+  echo "BIN:[" . $data . "]<br>";
+?>
+¤Ç¤¹¡£<br>
+
+<hr>
+
+<br>
+ýç<a href="index.php" accesskey="1">Ìá¤ë</a><br>
+  </body>
+</html>
