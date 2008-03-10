3dc643ffea5512c10467442bf3d12a7d7523596e
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-03-10 16:47:30 +0000

    *** empty log message ***
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_8_0@1865 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- include/qs_parse_string.h
+++ include/qs_parse_string.h
@@ -113,16 +113,16 @@ struct Attr {
 typedef struct Node Node;
 
 struct Node {
-  struct Node*   next;
-  struct Node*   parent;
-  struct Node*   child;
-  struct Node*   child_tail;
-  struct Attr*   attr;
-  struct Attr*   attr_tail;
-  char*          name;
-  char*          value;
+  struct Node   *next;
+  struct Node   *parent;
+  struct Node   *child;
+  struct Node   *child_tail;
+  struct Attr   *attr;
+  struct Attr   *attr_tail;
+  char          *name;
+  char          *value;
   int            size;
-  char*          otext;
+  char          *otext;
   int            line;
 };
 
@@ -140,33 +140,33 @@ typedef enum chxj_parse_mode_t {
 } ParseMode_t;
 
 typedef struct _doc {
-  Node*         now_parent_node;
-  Node*         root_node;
+  Node *now_parent_node;
+  Node *root_node;
 
   int           do_init_flag;
   unsigned long alloc_size;
 
-  Pointer_Table* pointer_table;
-  Pointer_Table* free_list_head;
-  Pointer_Table* free_list_tail;
-  Pointer_Table* allocated_list_head;
-  Pointer_Table* allocated_list_tail;
+  Pointer_Table *pointer_table;
+  Pointer_Table *free_list_head;
+  Pointer_Table *free_list_tail;
+  Pointer_Table *allocated_list_head;
+  Pointer_Table *allocated_list_tail;
 
   ParseMode_t    parse_mode;
 
-  apr_allocator_t* allocator;
-  apr_pool_t*      pool;
+  apr_allocator_t *allocator;
+  apr_pool_t      *pool;
 
 #ifndef __NON_MOD_CHXJ__
-  request_rec* r;
+  request_rec *r;
 #endif
 } Doc;
 
 /*
  * Prototype Declare
  */
-extern Node* qs_init_root_node(
-  Doc* doc);
+extern Node *qs_init_root_node(
+  Doc *doc);
 
 extern void qs_add_child_node(
   Doc* doc, 
