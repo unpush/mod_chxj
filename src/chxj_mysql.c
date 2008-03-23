49204e2ebcbc43f50803b6a33b17e2884efed03f
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-03-23 14:15:33 +0000

    * Added Features.     - save cookie to MySQL
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/trunk@1902 1a406e8e-add9-4483-a2c8-d8cac5b7c224

---
+++ src/chxj_mysql.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2005-2008 Atsushi Konno All rights reserved.
+ * Copyright (C) 2005 QSDN,Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifdef USE_MYSQL_COOKIE
+#include "mod_chxj.h"
+#include "chxj_cookie.h"
+#include "chxj_url_encode.h"
+#include "chxj_apply_convrule.h"
+
+#include "ap_release.h"
+
+#include "apu.h"
+#include "apr_dbm.h"
+#include "apr_uuid.h"
+#include "apr_md5.h"
+#include "apr_base64.h"
+#include "apr_uri.h"
+
+
+#endif
+/*
+ * vim:ts=2 et
+ */
