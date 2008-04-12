9c92f3a8144b5a4b9339b0594cff4d517f7f7e17
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-04-12 17:23:22 +0000

    * Added test code of the <base> tag for au XHTML converter.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_11_0@2134 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_xhtml_mobile_1_0/test_chxj_xhtml_mobile_1_0.c
+++ test/chxj_xhtml_mobile_1_0/test_chxj_xhtml_mobile_1_0.c
@@ -472,13 +472,13 @@ main()
   CU_add_test(xhtml_suite, "test a tag accesskey attribute with void char.",    test_xhtml_a_tag_accesskey_attribute_002);
   CU_add_test(xhtml_suite, "test a tag accesskey attribute with no value",      test_xhtml_a_tag_accesskey_attribute_003);
 
-#if 0
   CU_add_test(xhtml_suite, "test base tag no attribute.",                       test_xhtml_base_tag_001);
   CU_add_test(xhtml_suite, "test base tag href attribute with no value.",       test_xhtml_base_tag_href_attribute_001);
   CU_add_test(xhtml_suite, "test base tag href attribute with void value.",     test_xhtml_base_tag_href_attribute_002);
   CU_add_test(xhtml_suite, "test base tag href attribute with normal value.",   test_xhtml_base_tag_href_attribute_003);
   CU_add_test(xhtml_suite, "test base tag href attribute with normal value.",   test_xhtml_base_tag_href_attribute_004);
 
+#if 0
   CU_add_test(xhtml_suite, "test <blockquote> with void value.",                test_xhtml_blockquote_tag_001);
   CU_add_test(xhtml_suite, "test <blockquote> with value.",                     test_xhtml_blockquote_tag_002);
   CU_add_test(xhtml_suite, "test <blockquote> with japanese value.",            test_xhtml_blockquote_tag_003);
