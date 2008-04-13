77c6fa70461714ef74d9effa48ff843d43f999be
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-04-13 10:12:26 +0000

    * Added test code of the <body> tag 3 for au XHTML converter.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_11_0@2165 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_xhtml_mobile_1_0/test_chxj_xhtml_mobile_1_0.c
+++ test/chxj_xhtml_mobile_1_0/test_chxj_xhtml_mobile_1_0.c
@@ -485,8 +485,8 @@ main()
 
   CU_add_test(xhtml_suite, "test <body> .",                                     test_xhtml_body_tag_001);
   CU_add_test(xhtml_suite, "test <body> with bgcolor attribute 1.",             test_xhtml_body_tag_002);
-#if 0
   CU_add_test(xhtml_suite, "test <body> with bgcolor attribute 2.",             test_xhtml_body_tag_003);
+#if 0
   CU_add_test(xhtml_suite, "test <body> with bgcolor attribute 3.",             test_xhtml_body_tag_004);
   CU_add_test(xhtml_suite, "test <body> with text attribute 1.",                test_xhtml_body_tag_005);
   CU_add_test(xhtml_suite, "test <body> with text attribute 2.",                test_xhtml_body_tag_006);
@@ -1703,7 +1703,6 @@ void test_xhtml_body_tag_001()
 #undef TEST_STRING
 #undef RESULT_STRING
 }
-/* KONNO */
 void test_xhtml_body_tag_002() 
 {
 #define  TEST_STRING "<body bgcolor></body>"
@@ -1734,8 +1733,8 @@ void test_xhtml_body_tag_002()
 }
 void test_xhtml_body_tag_003() 
 {
-#define  TEST_STRING "<html><head></head><body bgcolor=\"\"></body></html>"
-#define  RESULT_STRING "<html><head></head><body></body></html>"
+#define  TEST_STRING "<body bgcolor=\"\"></body>"
+#define  RESULT_STRING "<body>\r\n</body>\r\n"
   char  *ret;
   char  *tmp;
   device_table spec;
@@ -1760,6 +1759,7 @@ void test_xhtml_body_tag_003()
 #undef TEST_STRING
 #undef RESULT_STRING
 }
+/* KONNO */
 void test_xhtml_body_tag_004() 
 {
 #define  TEST_STRING "<html><head></head><body bgcolor=\"#ff0000\"></body></html>"
