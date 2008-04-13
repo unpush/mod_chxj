ad76745e6a3a6633fe02418aaaf352fa25b33fba
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-04-13 10:12:17 +0000

    * Added test code of the <body> tag 1 for au XHTML converter.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_11_0@2163 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_xhtml_mobile_1_0/test_chxj_xhtml_mobile_1_0.c
+++ test/chxj_xhtml_mobile_1_0/test_chxj_xhtml_mobile_1_0.c
@@ -482,9 +482,9 @@ main()
   CU_add_test(xhtml_suite, "test <blockquote> with value.",                     test_xhtml_blockquote_tag_002);
   CU_add_test(xhtml_suite, "test <blockquote> with japanese value.",            test_xhtml_blockquote_tag_003);
   CU_add_test(xhtml_suite, "test <blockquote> with hankaku kana value.",        test_xhtml_blockquote_tag_004);
-#if 0
 
   CU_add_test(xhtml_suite, "test <body> .",                                     test_xhtml_body_tag_001);
+#if 0
   CU_add_test(xhtml_suite, "test <body> with bgcolor attribute 1.",             test_xhtml_body_tag_002);
   CU_add_test(xhtml_suite, "test <body> with bgcolor attribute 2.",             test_xhtml_body_tag_003);
   CU_add_test(xhtml_suite, "test <body> with bgcolor attribute 3.",             test_xhtml_body_tag_004);
@@ -1676,8 +1676,8 @@ void test_xhtml_blockquote_tag_004()
 /*============================================================================*/
 void test_xhtml_body_tag_001() 
 {
-#define  TEST_STRING "<html><head></head><body></body></html>"
-#define  RESULT_STRING "<html><head></head><body></body></html>"
+#define  TEST_STRING "<body></body>"
+#define  RESULT_STRING "<body>\r\n</body>\r\n"
   char  *ret;
   char  *tmp;
   device_table spec;
@@ -1694,6 +1694,7 @@ void test_xhtml_body_tag_001()
   tmp = chxj_encoding(&r, TEST_STRING, &destlen);
   ret = chxj_exchange_xhtml_mobile_1_0(&r, &spec, tmp, destlen, &destlen, &entry, &cookie);
   ret = chxj_rencoding(&r, ret, &destlen);
+  fprintf(stderr, "ret=[%s]",ret);
   CU_ASSERT(ret != NULL);
   CU_ASSERT(strcmp(RESULT_STRING, ret) == 0);
   CU_ASSERT(destlen == sizeof(RESULT_STRING)-1);
