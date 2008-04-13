58818e787d6eda504917db1cc23b368cc07b588d
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-04-13 10:15:59 +0000

    * Added test code of the <form> tag 2 for au XHTML converter.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_11_0@2206 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_xhtml_mobile_1_0/test_chxj_xhtml_mobile_1_0.c
+++ test/chxj_xhtml_mobile_1_0/test_chxj_xhtml_mobile_1_0.c
@@ -529,8 +529,8 @@ main()
   CU_add_test(xhtml_suite, "test <div> with style attribute.",                  test_xhtml_div_tag_007);
 
   CU_add_test(xhtml_suite, "test <form>.",                                      test_xhtml_form_tag_001);
-#if 0
   CU_add_test(xhtml_suite, "test <form method>.",                               test_xhtml_form_tag_002);
+#if 0
   CU_add_test(xhtml_suite, "test <form method=\"post\">.",                      test_xhtml_form_tag_003);
   CU_add_test(xhtml_suite, "test <form method=\"get\">.",                       test_xhtml_form_tag_004);
   CU_add_test(xhtml_suite, "test <form method=\"abc\">.",                       test_xhtml_form_tag_005);
@@ -2800,11 +2800,10 @@ void test_xhtml_form_tag_001()
 #undef TEST_STRING
 #undef RESULT_STRING
 }
-/* KONNO */
 void test_xhtml_form_tag_002() 
 {
-#define  TEST_STRING "<html><head></head><body><form method></form></body></html>"
-#define  RESULT_STRING "<html><head></head><body><form method=\"\"></form></body></html>"
+#define  TEST_STRING "<form method></form>"
+#define  RESULT_STRING "<form method=\"\"></form>"
   char  *ret;
   char  *tmp;
   device_table spec;
@@ -2821,6 +2820,7 @@ void test_xhtml_form_tag_002()
   tmp = chxj_encoding(&r, TEST_STRING, &destlen);
   ret = chxj_exchange_xhtml_mobile_1_0(&r, &spec, tmp, destlen, &destlen, &entry, &cookie);
   ret = chxj_rencoding(&r, ret, &destlen);
+  fprintf(stderr, "ret=[%s]",ret);
   CU_ASSERT(ret != NULL);
   CU_ASSERT(strcmp(RESULT_STRING, ret) == 0);
   CU_ASSERT(destlen == sizeof(RESULT_STRING)-1);
@@ -2829,6 +2829,7 @@ void test_xhtml_form_tag_002()
 #undef TEST_STRING
 #undef RESULT_STRING
 }
+/* KONNO */
 void test_xhtml_form_tag_003() 
 {
 #define  TEST_STRING "<html><head></head><body><form method=\"post\"></form></body></html>"
