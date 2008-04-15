52b5a2f41ffd5b01bb9cc170458a0eb991d03e36
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-04-15 16:47:14 +0000

    * Added test code of the <dt> tag for au HDML converter.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_11_0@2282 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_hdml/test_chxj_hdml.c
+++ test/chxj_hdml/test_chxj_hdml.c
@@ -555,13 +555,13 @@ main()
   /*=========================================================================*/
   CU_add_test(hdml_suite, "test <dl>.",                                        test_hdml_dl_tag_001);
 
-#if 0
   /*=========================================================================*/
   /* <DT>                                                                    */
   /*=========================================================================*/
   CU_add_test(hdml_suite, "test <dt>.",                                        test_hdml_dt_tag_001);
   CU_add_test(hdml_suite, "test <dt> with void value.",                        test_hdml_dt_tag_002);
 
+#if 0
   /*=========================================================================*/
   /* <DD>                                                                    */
   /*=========================================================================*/
@@ -2800,14 +2800,13 @@ void test_hdml_dl_tag_001()
 #undef TEST_STRING
 #undef RESULT_STRING
 }
-/* KONNO */
 /*============================================================================*/
 /* <DT>                                                                       */
 /*============================================================================*/
 void test_hdml_dt_tag_001()
 {
 #define  TEST_STRING "<dl><dt>あああ</dt><dt>いいい</dt></dl>"
-#define  RESULT_STRING "<dl><dt>あああ</dt><dt>いいい</dt></dl>"
+#define  RESULT_STRING "<LINE>あああ\r\n<LINE>いいい\r\n"
   char  *ret;
   char  *tmp;
   device_table spec;
@@ -2824,7 +2823,8 @@ void test_hdml_dt_tag_001()
   tmp = chxj_encoding(&r, TEST_STRING, &destlen);
   ret = chxj_exchange_hdml(&r, &spec, tmp, destlen, &destlen, &entry, &cookie);
   ret = chxj_rencoding(&r, ret, &destlen);
-  fprintf(stderr, "ret=[%s]",ret);
+  fprintf(stderr, "actual=[%s]\n", ret);
+  fprintf(stderr, "except=[%s]\n", RESULT_STRING);
   CU_ASSERT(ret != NULL);
   CU_ASSERT(strcmp(RESULT_STRING, ret) == 0);
   CU_ASSERT(destlen == sizeof(RESULT_STRING)-1);
@@ -2836,7 +2836,7 @@ void test_hdml_dt_tag_001()
 void test_hdml_dt_tag_002()
 {
 #define  TEST_STRING "<dl><dt></dt><dt></dt></dl>"
-#define  RESULT_STRING "<dl><dt></dt><dt></dt></dl>"
+#define  RESULT_STRING "<LINE>\r\n<LINE>\r\n"
   char  *ret;
   char  *tmp;
   device_table spec;
@@ -2853,7 +2853,8 @@ void test_hdml_dt_tag_002()
   tmp = chxj_encoding(&r, TEST_STRING, &destlen);
   ret = chxj_exchange_hdml(&r, &spec, tmp, destlen, &destlen, &entry, &cookie);
   ret = chxj_rencoding(&r, ret, &destlen);
-  fprintf(stderr, "ret=[%s]",ret);
+  fprintf(stderr, "actual=[%s]\n", ret);
+  fprintf(stderr, "except=[%s]\n", RESULT_STRING);
   CU_ASSERT(ret != NULL);
   CU_ASSERT(strcmp(RESULT_STRING, ret) == 0);
   CU_ASSERT(destlen == sizeof(RESULT_STRING)-1);
@@ -2862,6 +2863,7 @@ void test_hdml_dt_tag_002()
 #undef TEST_STRING
 #undef RESULT_STRING
 }
+/* KONNO */
 /*============================================================================*/
 /* <DD>                                                                       */
 /*============================================================================*/
