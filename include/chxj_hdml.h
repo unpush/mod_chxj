42a95dfdb52c26598b8270f2939cd2dac03912a1
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-04-15 16:48:08 +0000

    * novice.   * added plaintext_len attribute.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_11_0@2304 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- include/chxj_hdml.h
+++ include/chxj_hdml.h
@@ -35,11 +35,11 @@
 typedef struct hdml_t hdml_t;
 
 struct hdml_t {
-  Doc*           doc;
-  char*          out;
+  Doc            *doc;
+  char           *out;
   int            center;
   int            in_center;
-  char*          card;
+  char           *card;
   int            card_cnt;
   apr_time_t     form_cnt;
 
@@ -53,26 +53,26 @@ struct hdml_t {
   int            pure_form_cnt;
 
   int     var_cnt[MAX_FORM_COUNT];
-  char*   postdata[MAX_FORM_COUNT];
+  char    *postdata[MAX_FORM_COUNT];
   /*------------------------------*/
   /* max <input type="radio"> tag */
   /*------------------------------*/
-  char*   radio_value_list[MAX_RADIO_COUNT][MAX_RADIO_VALUE_COUNT];
-  char*   radio_name_list[MAX_RADIO_COUNT];
+  char    *radio_value_list[MAX_RADIO_COUNT][MAX_RADIO_VALUE_COUNT];
+  char    *radio_name_list[MAX_RADIO_COUNT];
   int     radio_out_cnt[MAX_RADIO_COUNT];
-  char*   radio_checked_value[MAX_RADIO_COUNT];
+  char    *radio_checked_value[MAX_RADIO_COUNT];
   /*---------------------------------*/
   /* for <input type="submit"> tag   */
   /*---------------------------------*/
-  char*   submit_button[MAX_SUBMIT_BUTTON_COUNT];
+  char    *submit_button[MAX_SUBMIT_BUTTON_COUNT];
 
   /*---------------------------------*/
   /* for <input type="checkbox"> tag */
   /*---------------------------------*/
   int     has_checkbox;
 
-  char*   form_tmp;
-  char*   init_vars;
+  char    *form_tmp;
+  char    *init_vars;
   int     out_len;
   int     card_len;
 
@@ -88,38 +88,38 @@ struct hdml_t {
   int     hdml_br_flag;
   int     hdml_blockquote_flag;
 
-  int                 hdml_end_flag;
+  int     hdml_end_flag;
   int     dir_level;
+  int     plaintext_value_len;
 
-  device_table*       spec;
+  device_table     *spec;
+  mod_chxj_config  *conf;
 
-  mod_chxj_config*    conf;
-
-  chxjconvrule_entry* entryp;
-  cookie_t*           cookie;
+  chxjconvrule_entry *entryp;
+  cookie_t           *cookie;
 };
 
 
 /*============================================================================*/
 /* Prototype declaration.                                                     */
 /*============================================================================*/
-extern char* chxj_exchange_hdml(
-  request_rec*        r, 
-  device_table*       spec,
-  const char*         src,
+extern char *chxj_exchange_hdml(
+  request_rec         *r, 
+  device_table        *spec,
+  const char          *src,
   apr_size_t          srclen,
-  apr_size_t*         dstlen,
-  chxjconvrule_entry* entryp,
-  cookie_t*           cookie);
+  apr_size_t          *dstlen,
+  chxjconvrule_entry  *entryp,
+  cookie_t            *cookie);
 
-extern char* qs_out_apr_pstrcat(
-  request_rec* r, 
-  char*        o, 
-  char*        s, 
-  int*         len);
+extern char *qs_out_apr_pstrcat(
+  request_rec  *r, 
+  char         *o, 
+  char         *s, 
+  int          *len);
 
-extern char* qs_conv_istyle_to_format(
-  request_rec* r, 
-  char*        is);
+extern char *qs_conv_istyle_to_format(
+  request_rec  *r, 
+  char         *is);
 
 #endif
