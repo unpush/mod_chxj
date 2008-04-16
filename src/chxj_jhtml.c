f7dd1f640a9ed71a022bee735b732cb36cba05c2
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-04-16 18:07:36 +0000

    * Added <blockquote> tag for SoftBank HTML converter.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_11_0@2336 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_jhtml.c
+++ src/chxj_jhtml.c
@@ -82,6 +82,8 @@ static char *s_jhtml_start_b_tag        (void *pdoc, Node *node);
 static char *s_jhtml_end_b_tag          (void *pdoc, Node *node);
 static char *s_jhtml_chxjif_tag         (void *pdoc, Node *node); 
 static char *s_jhtml_text_tag           (void *pdoc, Node *node);
+static char *s_jhtml_start_blockquote_tag(void *pdoc, Node *node);
+static char *s_jhtml_end_blockquote_tag  (void *pdoc, Node *node);
 
 static void  s_init_jhtml(jhtml_t *jhtml, Doc *doc, request_rec *r, device_table *spec);
 
@@ -319,8 +321,8 @@ tag_handler jhtml_handler[] = {
   },
   /* tagBLOCKQUOTE */
   {
-    NULL,
-    NULL,
+    s_jhtml_start_blockquote_tag,
+    s_jhtml_end_blockquote_tag,
   },
   /* tagDIR */
   {
@@ -2405,6 +2407,43 @@ s_jhtml_text_tag(void* pdoc, Node* child)
   W_V(tdst);
   return jhtml->out;
 }
+
+
+/**
+ * It is a handler who processes the BLOCKQUOTE tag.
+ *
+ * @param pdoc  [i/o] The pointer to the JHTML structure at the output
+ *                     destination is specified.
+ * @param node   [i]   The BLOCKQUOTE tag node is specified.
+ * @return The conversion result is returned.
+ */
+static char *
+s_jhtml_start_blockquote_tag(void *pdoc, Node *UNUSED(child))
+{
+  jhtml_t *jhtml = GET_JHTML(pdoc);
+  Doc     *doc   = jhtml->doc;
+  W_L("<blockquote>");
+  return jhtml->out;
+}
+
+
+/**
+ * It is a handler who processes the BLOCKQUOTE tag.
+ *
+ * @param pdoc  [i/o] The pointer to the JHTML structure at the output
+ *                     destination is specified.
+ * @param node   [i]   The BLOCKQUOTE tag node is specified.
+ * @return The conversion result is returned.
+ */
+static char *
+s_jhtml_end_blockquote_tag(void *pdoc, Node *UNUSED(child))
+{
+  jhtml_t *jhtml = GET_JHTML(pdoc);
+  Doc     *doc   = jhtml->doc;
+  W_L("</blockquote>");
+  return jhtml->out;
+}
+
 /*
  * vim:ts=2 et
  */
