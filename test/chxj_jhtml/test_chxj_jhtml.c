812429869176675705a4e0e6bdb3c9d8c0cf07ff
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-04-16 18:09:28 +0000

    * Added test code of the <dd> tag for SoftBank HTML converter.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_11_0@2349 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_jhtml/test_chxj_jhtml.c
+++ test/chxj_jhtml/test_chxj_jhtml.c
@@ -547,10 +547,13 @@ main()
   CU_add_test(jhtml_suite, "test <dt>.",                                        test_jhtml_dt_tag_001);
   CU_add_test(jhtml_suite, "test <dt> with void value.",                        test_jhtml_dt_tag_002);
 
-#if 0
+  /*=========================================================================*/
+  /* <DD>                                                                    */
+  /*=========================================================================*/
   CU_add_test(jhtml_suite, "test <dd>.",                                        test_jhtml_dd_tag_001);
   CU_add_test(jhtml_suite, "test <dd> with void value.",                        test_jhtml_dd_tag_002);
 
+#if 0
   CU_add_test(jhtml_suite, "test <div>.",                                       test_jhtml_div_tag_001);
   CU_add_test(jhtml_suite, "test <div> with align attribute(left).",            test_jhtml_div_tag_002);
   CU_add_test(jhtml_suite, "test <div> with align attribute(right).",           test_jhtml_div_tag_003);
@@ -2579,7 +2582,6 @@ void test_jhtml_dt_tag_002()
 #undef TEST_STRING
 #undef RESULT_STRING
 }
-/* KONNO */
 /*============================================================================*/
 /* <DD>                                                                       */
 /*============================================================================*/
@@ -2603,6 +2605,8 @@ void test_jhtml_dd_tag_001()
   tmp = chxj_encoding(&r, TEST_STRING, &destlen);
   ret = chxj_exchange_jhtml(&r, &spec, tmp, destlen, &destlen, &entry, &cookie);
   ret = chxj_rencoding(&r, ret, &destlen);
+  fprintf(stderr, "actual:[%s]\n", ret);
+  fprintf(stderr, "expect:[%s]\n", RESULT_STRING);
   CU_ASSERT(ret != NULL);
   CU_ASSERT(strcmp(RESULT_STRING, ret) == 0);
   CU_ASSERT(destlen == sizeof(RESULT_STRING)-1);
@@ -2631,6 +2635,8 @@ void test_jhtml_dd_tag_002()
   tmp = chxj_encoding(&r, TEST_STRING, &destlen);
   ret = chxj_exchange_jhtml(&r, &spec, tmp, destlen, &destlen, &entry, &cookie);
   ret = chxj_rencoding(&r, ret, &destlen);
+  fprintf(stderr, "actual:[%s]\n", ret);
+  fprintf(stderr, "expect:[%s]\n", RESULT_STRING);
   CU_ASSERT(ret != NULL);
   CU_ASSERT(strcmp(RESULT_STRING, ret) == 0);
   CU_ASSERT(destlen == sizeof(RESULT_STRING)-1);
@@ -2639,6 +2645,7 @@ void test_jhtml_dd_tag_002()
 #undef TEST_STRING
 #undef RESULT_STRING
 }
+/* KONNO */
 /*============================================================================*/
 /* <DIV>                                                                      */
 /*============================================================================*/
