0e565f91a71f98de8997be4271d17e84fd9ff8f5
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-04-21 17:07:31 +0000

    * novice.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_12_0@2422 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_img_conv.c
+++ src/chxj_img_conv.c
@@ -17,16 +17,13 @@
 #include "mod_chxj.h"
 
 
-char* 
+char * 
 chxj_img_conv(
-  request_rec*  r, 
-  device_table* spec, 
-  const char*   s)
+  request_rec  *r, 
+  device_table *spec, 
+  const char   *s)
 {
-  char* dst;
-
-  dst = apr_pstrdup(r->pool, s);
-
+  char *dst = apr_pstrdup(r->pool, s);
   if (spec) {
     switch (spec->color) {
     case 2       : dst = apr_pstrcat(r->pool, dst,".10", NULL); break;
