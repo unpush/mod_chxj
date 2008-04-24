c9118fc6682d823a9ba66d79c3ba4e7323e01371
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-04-24 18:07:04 +0000

    * Added <dir type> attribute for au XHTML converter.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_12_0@2495 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_xhtml_mobile_1_0.c
+++ src/chxj_xhtml_mobile_1_0.c
@@ -2790,11 +2790,26 @@ s_xhtml_1_0_end_blockquote_tag(void *pdoc, Node *UNUSED(child))
  * @return The conversion result is returned.
  */
 static char *
-s_xhtml_1_0_start_dir_tag(void *pdoc, Node *UNUSED(child))
+s_xhtml_1_0_start_dir_tag(void *pdoc, Node *node)
 {
   xhtml_t *xhtml = GET_XHTML(pdoc);
   Doc     *doc = xhtml->doc;
-  W_L("<dir>");
+  Attr      *attr;
+  W_L("<dir");
+  for (attr = qs_get_attr(doc,node);
+       attr;
+       attr = qs_get_next_attr(doc,attr)) {
+    char *name   = qs_get_attr_name(doc,attr);
+    char *value  = qs_get_attr_value(doc,attr);
+    if (STRCASEEQ('t','T',"type",name)) {
+      if (value && (STRCASEEQ('d','D',"disc",value) || STRCASEEQ('c','C',"circle",value) || STRCASEEQ('s','S',"square",value))) {
+        W_L(" type=\"");
+        W_V(value);
+        W_L("\"");
+      }
+    }
+  }
+  W_L(">");
   return xhtml->out;
 }
 
