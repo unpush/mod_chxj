435a97f72a723cddec45daf358b5aed6662be436
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-04-25 18:16:10 +0000

    * Added <menu type> attribute for SoftBank XHTML converter.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_12_0@2511 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_jxhtml.c
+++ src/chxj_jxhtml.c
@@ -3198,11 +3198,29 @@ s_jxhtml_end_h6_tag(void *pdoc, Node *UNUSED(child))
  * @return The conversion result is returned.
  */
 static char *
-s_jxhtml_start_menu_tag(void *pdoc, Node *UNUSED(child))
+s_jxhtml_start_menu_tag(void *pdoc, Node *node)
 {
   jxhtml_t *jxhtml = GET_JXHTML(pdoc);
-  Doc     *doc = jxhtml->doc;
-  W_L("<menu>");
+  Doc      *doc = jxhtml->doc;
+  Attr     *attr;
+  W_L("<menu");
+  /*--------------------------------------------------------------------------*/
+  /* Get Attributes                                                           */
+  /*--------------------------------------------------------------------------*/
+  for (attr = qs_get_attr(doc,node);
+       attr;
+       attr = qs_get_next_attr(doc,attr)) {
+    char *name   = qs_get_attr_name(doc,attr);
+    char *value  = qs_get_attr_value(doc,attr);
+    if (STRCASEEQ('t','T',"type",name)) {
+      if (value && (STRCASEEQ('d','D',"disc",value) || STRCASEEQ('c','C',"circle",value) || STRCASEEQ('s','S',"square",value))) {
+        W_L(" type=\"");
+        W_V(value);
+        W_L("\"");
+      }
+    }
+  }
+  W_L(">");
   return jxhtml->out;
 }
 
