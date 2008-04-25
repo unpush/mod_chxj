59c95f40b88948f8a6698af0956ce83fa038bf14
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-04-25 14:02:24 +0000

    * Added test code of the <menu type> attribute for CHTML1.0 converter.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_12_0@2500 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_chtml10/test_chxj_chtml10.c
+++ test/chxj_chtml10/test_chxj_chtml10.c
@@ -291,6 +291,9 @@ void test_chtml10_menu_tag_002();
 void test_chtml10_menu_tag_003(); 
 void test_chtml10_menu_tag_004(); 
 void test_chtml10_menu_tag_005(); 
+void test_chtml10_menu_tag_006(); 
+void test_chtml10_menu_tag_007(); 
+void test_chtml10_menu_tag_008(); 
 
 void test_chtml10_ol_tag_001(); 
 void test_chtml10_ol_tag_002(); 
@@ -638,6 +641,9 @@ main()
   CU_add_test(chtml10_suite, "test <menu> 3." ,                                   test_chtml10_menu_tag_003); 
   CU_add_test(chtml10_suite, "test <menu> 4." ,                                   test_chtml10_menu_tag_004); 
   CU_add_test(chtml10_suite, "test <menu> 5." ,                                   test_chtml10_menu_tag_005); 
+  CU_add_test(chtml10_suite, "test <menu> 6." ,                                   test_chtml10_menu_tag_006); 
+  CU_add_test(chtml10_suite, "test <menu> 7." ,                                   test_chtml10_menu_tag_007); 
+  CU_add_test(chtml10_suite, "test <menu> 8." ,                                   test_chtml10_menu_tag_008); 
 
   CU_add_test(chtml10_suite, "test <ol>." ,                                       test_chtml10_ol_tag_001); 
   CU_add_test(chtml10_suite, "test <ol> 2." ,                                     test_chtml10_ol_tag_002); 
@@ -7192,6 +7198,92 @@ void test_chtml10_menu_tag_004()
 void test_chtml10_menu_tag_005() 
 {
 #define  TEST_STRING "<menu><li>ﾊﾝｶｸ</li><li>ﾊﾝｶｸ</li></menu>"
+#define  RESULT_STRING "<menu><li>ﾊﾝｶｸ<li>ﾊﾝｶｸ</menu>"
+  char  *ret;
+  char  *tmp;
+  device_table spec;
+  chxjconvrule_entry entry;
+  cookie_t cookie;
+  apr_size_t destlen;
+  APR_INIT;
+
+  COOKIE_INIT(cookie);
+
+  SPEC_INIT(spec);
+  destlen = sizeof(TEST_STRING)-1;
+
+  tmp = chxj_encoding(&r, TEST_STRING, &destlen);
+  ret = chxj_convert_chtml10(&r, &spec, tmp, destlen, &destlen, &entry, &cookie);
+  ret = chxj_rencoding(&r, ret, &destlen);
+  CU_ASSERT(ret != NULL);
+  CU_ASSERT(strcmp(RESULT_STRING, ret) == 0);
+  CU_ASSERT(destlen == sizeof(RESULT_STRING)-1);
+
+  APR_TERM;
+#undef TEST_STRING
+#undef RESULT_STRING
+}
+/* "disc | circle | square" */
+void test_chtml10_menu_tag_006() 
+{
+#define  TEST_STRING "<menu type=\"disc\"><li>ﾊﾝｶｸ</li><li>ﾊﾝｶｸ</li></menu>"
+#define  RESULT_STRING "<menu><li>ﾊﾝｶｸ<li>ﾊﾝｶｸ</menu>"
+  char  *ret;
+  char  *tmp;
+  device_table spec;
+  chxjconvrule_entry entry;
+  cookie_t cookie;
+  apr_size_t destlen;
+  APR_INIT;
+
+  COOKIE_INIT(cookie);
+
+  SPEC_INIT(spec);
+  destlen = sizeof(TEST_STRING)-1;
+
+  tmp = chxj_encoding(&r, TEST_STRING, &destlen);
+  ret = chxj_convert_chtml10(&r, &spec, tmp, destlen, &destlen, &entry, &cookie);
+  ret = chxj_rencoding(&r, ret, &destlen);
+  CU_ASSERT(ret != NULL);
+  CU_ASSERT(strcmp(RESULT_STRING, ret) == 0);
+  CU_ASSERT(destlen == sizeof(RESULT_STRING)-1);
+
+  APR_TERM;
+#undef TEST_STRING
+#undef RESULT_STRING
+}
+void test_chtml10_menu_tag_007() 
+{
+#define  TEST_STRING "<menu type=\"circle\"><li>ﾊﾝｶｸ</li><li>ﾊﾝｶｸ</li></menu>"
+#define  RESULT_STRING "<menu><li>ﾊﾝｶｸ<li>ﾊﾝｶｸ</menu>"
+  char  *ret;
+  char  *tmp;
+  device_table spec;
+  chxjconvrule_entry entry;
+  cookie_t cookie;
+  apr_size_t destlen;
+  APR_INIT;
+
+  COOKIE_INIT(cookie);
+
+  SPEC_INIT(spec);
+  destlen = sizeof(TEST_STRING)-1;
+
+  tmp = chxj_encoding(&r, TEST_STRING, &destlen);
+  ret = chxj_convert_chtml10(&r, &spec, tmp, destlen, &destlen, &entry, &cookie);
+  ret = chxj_rencoding(&r, ret, &destlen);
+  CU_ASSERT(ret != NULL);
+  CU_ASSERT(strcmp(RESULT_STRING, ret) == 0);
+  CU_ASSERT(destlen == sizeof(RESULT_STRING)-1);
+
+  APR_TERM;
+#undef TEST_STRING
+#undef RESULT_STRING
+}
+/* "disc | circle | square" */
+void test_chtml10_menu_tag_008() 
+{
+#define  TEST_STRING "<menu type=\"square\"><li>ﾊﾝｶｸ</li><li>ﾊﾝｶｸ</li></menu>"
 #define  RESULT_STRING "<menu><li>ﾊﾝｶｸ<li>ﾊﾝｶｸ</menu>"
   char  *ret;
   char  *tmp;
