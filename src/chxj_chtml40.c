398482cddb9dba59499e821bd97928d77d61edd3
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-04-26 08:29:02 +0000

    * Added <li type="disc"> <li type="circle"> <li type="square"> attribute for CHTML4.0 converter.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_12_0@2528 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_chtml40.c
+++ src/chxj_chtml40.c
@@ -2250,10 +2250,12 @@ s_chtml40_start_li_tag(void *pdoc, Node *node)
        attr = qs_get_next_attr(doc,attr)) {
     char *name = qs_get_attr_name(doc,attr);
     char *value = qs_get_attr_value(doc,attr);
-    if (STRCASEEQ('t','T',"type",name) && value && (*value == '1' || *value == 'a' || *value == 'A')) {
-      W_L(" type=\"");
-      W_V(value);
-      W_L("\"");
+    if (STRCASEEQ('t','T',"type",name)) {
+      if (value && (*value == '1' || *value == 'a' || *value == 'A' || STRCASEEQ('d','D',"disc",value) || STRCASEEQ('s','S',"square",value) || STRCASEEQ('c','C',"circle",value))) {
+        W_L(" type=\"");
+        W_V(value);
+        W_L("\"");
+      }
     }
     else if (STRCASEEQ('v','V',"value", name) && value && *value) {
       W_L(" value=\"");
