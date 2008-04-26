3c10780a0c785b00beda6f6f00e965021574ea16
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-04-26 12:55:37 +0000

    * Added test code of the <marquee bgcolor> attribute for SoftBank HTML converter.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_12_0@2543 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_jhtml/test_chxj_jhtml.c
+++ test/chxj_jhtml/test_chxj_jhtml.c
@@ -447,6 +447,9 @@ void test_jhtml_marquee_tag_015();
 void test_jhtml_marquee_tag_016();
 void test_jhtml_marquee_tag_017();
 void test_jhtml_marquee_tag_018();
+void test_jhtml_marquee_tag_019();
+void test_jhtml_marquee_tag_020();
+void test_jhtml_marquee_tag_021();
 
 void test_jhtml_meta_tag_001();
 void test_jhtml_meta_tag_002();
@@ -988,6 +991,9 @@ main()
   CU_add_test(jhtml_suite, "test <marquee> 16." ,                               test_jhtml_marquee_tag_016);
   CU_add_test(jhtml_suite, "test <marquee> 17." ,                               test_jhtml_marquee_tag_017);
   CU_add_test(jhtml_suite, "test <marquee> 18." ,                               test_jhtml_marquee_tag_018);
+  CU_add_test(jhtml_suite, "test <marquee> 19." ,                               test_jhtml_marquee_tag_019);
+  CU_add_test(jhtml_suite, "test <marquee> 20." ,                               test_jhtml_marquee_tag_020);
+  CU_add_test(jhtml_suite, "test <marquee> 21." ,                               test_jhtml_marquee_tag_021);
 
   /*=========================================================================*/
   /* <META>                                                                  */
@@ -12372,6 +12378,96 @@ void test_jhtml_marquee_tag_017()
 void test_jhtml_marquee_tag_018() 
 {
 #define  TEST_STRING "<marquee loop=\"1\">ﾊﾝｶｸ</marquee>"
+#define  RESULT_STRING "<marquee>ﾊﾝｶｸ</marquee>"
+  char  *ret;
+  char  *tmp;
+  device_table spec;
+  chxjconvrule_entry entry;
+  cookie_t cookie;
+  apr_size_t destlen;
+  APR_INIT;
+
+  COOKIE_INIT(cookie);
+
+  SPEC_INIT(spec);
+  destlen = sizeof(TEST_STRING)-1;
+
+  tmp = chxj_encoding(&r, TEST_STRING, &destlen);
+  ret = chxj_convert_jhtml(&r, &spec, tmp, destlen, &destlen, &entry, &cookie);
+  ret = chxj_rencoding(&r, ret, &destlen);
+  fprintf(stderr, "actual:[%s]\n", ret);
+  fprintf(stderr, "expect:[%s]\n", RESULT_STRING);
+  CU_ASSERT(ret != NULL);
+  CU_ASSERT(strcmp(RESULT_STRING, ret) == 0);
+  CU_ASSERT(destlen == sizeof(RESULT_STRING)-1);
+
+  APR_TERM;
+#undef TEST_STRING
+#undef RESULT_STRING
+}
+void test_jhtml_marquee_tag_019() 
+{
+#define  TEST_STRING "<marquee bgcolor=\"#ff0000\">ﾊﾝｶｸ</marquee>"
+#define  RESULT_STRING "<marquee>ﾊﾝｶｸ</marquee>"
+  char  *ret;
+  char  *tmp;
+  device_table spec;
+  chxjconvrule_entry entry;
+  cookie_t cookie;
+  apr_size_t destlen;
+  APR_INIT;
+
+  COOKIE_INIT(cookie);
+
+  SPEC_INIT(spec);
+  destlen = sizeof(TEST_STRING)-1;
+
+  tmp = chxj_encoding(&r, TEST_STRING, &destlen);
+  ret = chxj_convert_jhtml(&r, &spec, tmp, destlen, &destlen, &entry, &cookie);
+  ret = chxj_rencoding(&r, ret, &destlen);
+  fprintf(stderr, "actual:[%s]\n", ret);
+  fprintf(stderr, "expect:[%s]\n", RESULT_STRING);
+  CU_ASSERT(ret != NULL);
+  CU_ASSERT(strcmp(RESULT_STRING, ret) == 0);
+  CU_ASSERT(destlen == sizeof(RESULT_STRING)-1);
+
+  APR_TERM;
+#undef TEST_STRING
+#undef RESULT_STRING
+}
+void test_jhtml_marquee_tag_020() 
+{
+#define  TEST_STRING "<marquee bgcolor=\"\">ﾊﾝｶｸ</marquee>"
+#define  RESULT_STRING "<marquee>ﾊﾝｶｸ</marquee>"
+  char  *ret;
+  char  *tmp;
+  device_table spec;
+  chxjconvrule_entry entry;
+  cookie_t cookie;
+  apr_size_t destlen;
+  APR_INIT;
+
+  COOKIE_INIT(cookie);
+
+  SPEC_INIT(spec);
+  destlen = sizeof(TEST_STRING)-1;
+
+  tmp = chxj_encoding(&r, TEST_STRING, &destlen);
+  ret = chxj_convert_jhtml(&r, &spec, tmp, destlen, &destlen, &entry, &cookie);
+  ret = chxj_rencoding(&r, ret, &destlen);
+  fprintf(stderr, "actual:[%s]\n", ret);
+  fprintf(stderr, "expect:[%s]\n", RESULT_STRING);
+  CU_ASSERT(ret != NULL);
+  CU_ASSERT(strcmp(RESULT_STRING, ret) == 0);
+  CU_ASSERT(destlen == sizeof(RESULT_STRING)-1);
+
+  APR_TERM;
+#undef TEST_STRING
+#undef RESULT_STRING
+}
+void test_jhtml_marquee_tag_021() 
+{
+#define  TEST_STRING "<marquee bgcolor>ﾊﾝｶｸ</marquee>"
 #define  RESULT_STRING "<marquee>ﾊﾝｶｸ</marquee>"
   char  *ret;
   char  *tmp;
