1c3ffc25b0242c0b74cf3a12f9acb6f19bc4dc32
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-07-30 17:24:00 +0000

    * The correction of 0.11.10 is applied to 0.11.1-1.
    git-svn-id: http://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_11_1p1@3162 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_cookie.c
+++ src/chxj_cookie.c
@@ -720,6 +720,8 @@ chxj_add_cookie_parameter(request_rec* r, char* value, cookie_t* cookie)
 {
   char* qs;
   char* dst;
+  char *tmp;
+  char *mark = "";
 
   DBG(r, "start chxj_add_cookie_parameter() cookie_id=[%s]", (cookie) ? cookie->cookie_id : NULL);
 
@@ -736,12 +738,18 @@ chxj_add_cookie_parameter(request_rec* r, char* value, cookie_t* cookie)
     goto on_error;
   }
 
+  tmp = strchr(dst, '#');
+  if (tmp) {
+    mark = apr_pstrdup(r->pool, tmp);
+    *tmp = 0;
+  }
+
   qs = strchr(dst, '?');
   if (qs) {
-    dst = apr_psprintf(r->pool, "%s&%s=%s", dst, CHXJ_COOKIE_PARAM, cookie->cookie_id);
+    dst = apr_psprintf(r->pool, "%s&%s=%s%s", dst, CHXJ_COOKIE_PARAM, cookie->cookie_id, mark);
   }
   else {
-    dst = apr_psprintf(r->pool, "%s?%s=%s", dst, CHXJ_COOKIE_PARAM, cookie->cookie_id);
+    dst = apr_psprintf(r->pool, "%s?%s=%s%s", dst, CHXJ_COOKIE_PARAM, cookie->cookie_id, mark);
   }
 
   DBG(r, "end   chxj_add_cookie_parameter() dst=[%s]", dst);
